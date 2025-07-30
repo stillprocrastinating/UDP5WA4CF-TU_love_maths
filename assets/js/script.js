57dbdefda686f534595634fe33ab1f50518bb8dc
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-07-30 18:33:32 +0100

    Add DOMContentLoaded & button listeners draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Get the button elements and add event listeners to them
+	let buttons = document.getElementsByTagName("button");
+	for (let button of buttons) {
+		button.addEventListener("click", function() {
+			if (this.getAttribute("data-type") === "submit") {
+				alert("You clicked submit!");
+			} else {
+				let gameType = this.getAttribute("data-type");
+				alert(`You clicked ${gameType}`);
+			}
+		})
+	}
+
+})
+
 function runGame() {}
 
 function checkAnswer() {}
