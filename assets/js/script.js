f76cee31980a0acc41495c3aaa7a58cf341a3af6
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-07-30 19:33:14 +0100

    Add calculateCorrectAnswer(){addition} draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,18 +39,35 @@ function runGame(gameType) {
 
 function checkAnswer() {}
 
-function calculateCorrectAnswer() {}
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus, etc.)
+ * directly from the dom, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+
+	let operand1 = parseInt(document.getElementById("operand1").innerText);
+	let operand2 = parseInt(document.getElementById("operand2").innerText);
+	let operator = document.getElementById("operator").innerText;
+
+	if (operator === "+") {
+		return [operand1 + operand2, "addition"];
+	} else {
+		alert(`Unimplemented operator ${operator}`);
+		throw `Unimplemented operator ${operator}. Aborting!`;
+	}
+
+}
 
 function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion(operand1, operand2) {
+function displayAdditionQuestions(operand1, operand2) {
 	document.getElementById("operand1").textContent = operand1;
 	document.getElementById("operand2").textContent = operand2;
 	document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractionQuestion() {}
+function displaySubtractionQuestions() {}
 
-function displayMultiplicationQuestion() {}
+function displayMultiplicationQuestions() {}
