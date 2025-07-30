cee08091e8b412155e658b48465e7aff6d07e1a9
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-07-30 19:02:41 +0100

    Add runGame(){displayAdditionQuestion();error;} draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
 				alert("You clicked submit!");
 			} else {
 				let gameType = this.getAttribute("data-type");
-				alert(`You clicked ${gameType}`);
+				runGame(gameType);
 			}
 		})
 	}
+
+	runGame("addition");
 
 })
 
@@ -20,10 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the users' answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
+
 	// Creates two random numbers between 1 and 25
 	let num1 = Math.floor(Math.random() * 25) + 1;
 	let num2 = Math.floor(Math.random() * 25) + 1;
+
+	if (gameType === "addition") {
+		displayAdditionQuestion(num1, num2);
+	} else {
+		alert(`Unknown game type: ${gameType}`);
+		throw `Unknown game type: ${gameType}. Aborting!`;
+	}
+
 }
 
 function checkAnswer() {}
@@ -34,8 +45,12 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestions() {}
+function displayAdditionQuestion(operand1, operand2) {
+	document.getElementById("operand1").textContent = operand1;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "+";
+}
 
-function displaySubtractionQuestions() {}
+function displaySubtractionQuestion() {}
 
-function displayMultiplicationQuestions() {}
+function displayMultiplicationQuestion() {}
